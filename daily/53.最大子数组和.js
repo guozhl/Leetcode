6ea8d49ab5aafdbffbd32f1f8a5175c1cc9e28f2
guozl1 <guozl1@ziroom.com>
2022-03-03 17:24:34 +0800

    update: solve problem 53

---
+++ daily/53.最大子数组和.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=javascript
+ *
+ * [53] 最大子数组和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let res = nums[0];
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i]
+    if (sum > 0)
+      sum += num;
+    else
+      sum = num;
+    res = Math.max(res, sum);
+  }
+
+  return res;
+};
+// @lc code=end
