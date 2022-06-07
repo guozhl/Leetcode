0e0ae570864d99c2aa5a9ebf836ff0685673b479
guozhl <skye_luckyme@126.com>
2022-06-07 11:49:35 +0800

    189

---
+++ daily/189.轮转数组.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=javascript
+ *
+ * [189] 轮转数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  k = k % nums.length
+  let arr = nums.splice(nums.length - k)
+  nums.unshift(...arr)
+};
+// @lc code=end
